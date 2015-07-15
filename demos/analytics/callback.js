2fa5ac79ce1e3e383cea2c4d88e382781e487b5d
0xC70FF3 <0xC70FF3@users.noreply.github.com>
2015-07-15 15:31:31 +0200

    Update callback.js

--- demos/analytics/callback.js
+++ demos/analytics/callback.js
@@ -1,2 +1,10 @@
+function parse(val) {
+    var result = "Not found", tmp = [];
+    location.search.substr(1).split("&").forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
 dataLayer.push({'myValue': 'this_is_my_value_' + parse('value')});
 dataLayer.push({'event': 'custom.dataLayer.modified'});
